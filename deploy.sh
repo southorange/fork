58891fd79b359d3e84ead81338fcf63affc4d455
southorange <southorange@users.noreply.github.com>
2016-05-26 20:24:55 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 set -e
 
 #dependencies
-sudo apt-get update
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
-sudo apt-get install --no-install-recommends libboost-all-dev
-sudo apt-get install python-dev
-sudo apt-get install ffmpeg
+sudo apt-get update -y
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler -y
+sudo apt-get install --no-install-recommends libboost-all-dev -y
+sudo apt-get install python-dev -y
+sudo apt-get install ffmpeg -y
 
 wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
 tar zxvf glog-0.3.3.tar.gz
@@ -21,7 +21,7 @@ mkdir build && cd build
 export CXXFLAGS="-fPIC" && cmake .. && make VERBOSE=1
 make && make install
 
-git clone https://github.com/LMDB/lmdb
+yes | git clone https://github.com/LMDB/lmdb
 cd lmdb/libraries/liblmdb
 make && make install
 
